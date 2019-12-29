7411cc4136aa20da82d6d1271a52e1d966599176
Jakixcar <mertkok.78@users.noreply.github.com>
2019-12-30 02:37:50 +0300

    Proje

---
+++ src/hastanekayitsistemi/HastaneKayitSistemi.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hastanekayitsistemi;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+/**
+ *
+ * @author ABRA
+ */
+public class HastaneKayitSistemi {
+        
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GirisSecimi obj = new GirisSecimi();
+        obj.setVisible(true);
+        
+        }
+    
+}
